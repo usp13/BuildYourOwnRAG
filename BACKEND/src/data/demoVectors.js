ed6917e2243714b444086becc61572a03ec0e55e
Utsav Panchal <utsavpanchal1304@gmail.com>
2026-07-09 01:48:10 +0530

    Added DemoVectors.js

---
+++ BACKEND/src/data/demoVectors.js
@@ -0,0 +1,131 @@
+const demoVectors = [
+  //  CS 
+  {
+    id: "cs1",
+    label: "Binary Tree",
+    category: "CS",
+    vector: [0.9,0.8,0.7,0.6,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "cs2",
+    label: "Linked List",
+    category: "CS",
+    vector: [0.85,0.75,0.65,0.55,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "cs3",
+    label: "Hash Table",
+    category: "CS",
+    vector: [0.88,0.82,0.71,0.63,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "cs4",
+    label: "Graph Algorithm",
+    category: "CS",
+    vector: [0.91,0.79,0.73,0.58,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "cs5",
+    label: "Dynamic Programming",
+    category: "CS",
+    vector: [0.87,0.84,0.72,0.66,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+
+  // Math 
+  {
+    id: "math1",
+    label: "Calculus",
+    category: "Math",
+    vector: [0.1,0.1,0.1,0.1,0.9,0.8,0.7,0.6,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "math2",
+    label: "Linear Algebra",
+    category: "Math",
+    vector: [0.1,0.1,0.1,0.1,0.88,0.77,0.68,0.58,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "math3",
+    label: "Probability",
+    category: "Math",
+    vector: [0.1,0.1,0.1,0.1,0.92,0.81,0.69,0.61,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "math4",
+    label: "Differential Equations",
+    category: "Math",
+    vector: [0.1,0.1,0.1,0.1,0.89,0.82,0.71,0.59,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "math5",
+    label: "Statistics",
+    category: "Math",
+    vector: [0.1,0.1,0.1,0.1,0.87,0.76,0.65,0.57,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+  },
+
+  // Food 
+  {
+    id: "food1",
+    label: "Pizza",
+    category: "Food",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9,0.8,0.7,0.6,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "food2",
+    label: "Burger",
+    category: "Food",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.88,0.79,0.69,0.58,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "food3",
+    label: "Pasta",
+    category: "Food",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.91,0.81,0.72,0.61,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "food4",
+    label: "Sushi",
+    category: "Food",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.89,0.77,0.66,0.55,0.1,0.1,0.1,0.1]
+  },
+  {
+    id: "food5",
+    label: "Ice Cream",
+    category: "Food",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.87,0.76,0.65,0.56,0.1,0.1,0.1,0.1]
+  },
+
+  // Sports
+  {
+    id: "sports1",
+    label: "Football",
+    category: "Sports",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.9,0.8,0.7,0.6]
+  },
+  {
+    id: "sports2",
+    label: "Basketball",
+    category: "Sports",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.88,0.79,0.68,0.57]
+  },
+  {
+    id: "sports3",
+    label: "Cricket",
+    category: "Sports",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.91,0.81,0.72,0.63]
+  },
+  {
+    id: "sports4",
+    label: "Tennis",
+    category: "Sports",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.89,0.77,0.66,0.58]
+  },
+  {
+    id: "sports5",
+    label: "Swimming",
+    category: "Sports",
+    vector: [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.86,0.75,0.67,0.55]
+  }
+];
+
+export default demoVectors;
